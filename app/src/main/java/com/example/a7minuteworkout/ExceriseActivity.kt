3f8b76846e4d17a4f81aee8cb863f770e2dbec76
rafi kyum <kyumrafi@gmail.com>
2021-08-24 18:21:01 +0100

    Timer working

--- app/src/main/java/com/example/a7minuteworkout/ExceriseActivity.kt
+++ app/src/main/java/com/example/a7minuteworkout/ExceriseActivity.kt
@@ -2,9 +2,19 @@ package com.example.a7minuteworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.ProgressBar
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
+import org.w3c.dom.Text
 
 class ExceriseActivity : AppCompatActivity() {
+
+    private var restTimer: CountDownTimer? = null
+    private var restProgress = 0
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_excerise)
@@ -18,7 +28,52 @@ class ExceriseActivity : AppCompatActivity() {
         toolbar_exericse_activity.setNavigationOnClickListener {
             onBackPressed()
         }
+
+        setUpRestView()
+
     }
+
+    private fun setUpRestView(){
+        if(restTimer != null){
+            restTimer!!.cancel()
+            restProgress = 0;
+        }
+
+        setRestProgressBar()
+    }
+
+
+
+    private fun setRestProgressBar() {
+        val progressBar:ProgressBar = findViewById(R.id.progressBar)
+        val tvTimer: TextView = findViewById(R.id.tvTimer)
+        progressBar.progress = restProgress
+        restTimer = object: CountDownTimer(10000, 1000){
+            override fun onTick(p0: Long) {
+                restProgress++
+                progressBar.progress = 10-restProgress
+                tvTimer.text = (10-restProgress).toString()
+            }
+
+            override fun onFinish() {
+               Toast.makeText(this@ExceriseActivity,
+               "Here now we will start the exercise.",
+               Toast.LENGTH_SHORT
+               ).show()
+            }
+        }.start()
+    }
+
+
+
+    override fun onDestroy() {
+        if(restTimer !=null){
+            restTimer!!.cancel()
+            restProgress = 0
+        }
+        super.onDestroy()
+    }
+
 
 
 }
