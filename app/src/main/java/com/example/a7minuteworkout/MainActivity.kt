4a10e6c7b8ffe59b5e418e3b8931c1f1bc900be6
rafi kyum <kyumrafi@gmail.com>
2021-08-24 17:09:52 +0100

    Start button implemented

--- app/src/main/java/com/example/a7minuteworkout/MainActivity.kt
+++ app/src/main/java/com/example/a7minuteworkout/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.a7minuteworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val llStart:LinearLayout = findViewById(R.id.llStart)
+        llStart.setOnClickListener{
+            Toast.makeText(this, "Here we will start the exercise", Toast.LENGTH_SHORT).show()
+        }
+
+
     }
 }
